81e018d353c3daddc6478f19037fd2f84e5481fa
Bennycopter <ben@natural20design.com>
2023-12-04 14:15:09 -0500

    Update spacers to match Tailwind

--- demo/dist/js/tabler.js
+++ demo/dist/js/tabler.js
@@ -3869,7 +3869,7 @@
 	});
 
 	/*!
-	  * Bootstrap v5.3.1 (https://getbootstrap.com/)
+	  * Bootstrap v5.3.2 (https://getbootstrap.com/)
 	  * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
 	  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
 	  */
@@ -4376,7 +4376,7 @@
 	    }
 	  }
 	}
-	const VERSION = '5.3.1';
+	const VERSION = '5.3.2';
 	class BaseComponent extends Config {
 	  constructor(element, config) {
 	    super();
@@ -4433,9 +4433,9 @@
 	    if (hrefAttribute.includes('#') && !hrefAttribute.startsWith('#')) {
 	      hrefAttribute = `#${hrefAttribute.split('#')[1]}`;
 	    }
-	    selector = hrefAttribute && hrefAttribute !== '#' ? hrefAttribute.trim() : null;
+	    selector = hrefAttribute && hrefAttribute !== '#' ? parseSelector(hrefAttribute.trim()) : null;
 	  }
-	  return parseSelector(selector);
+	  return selector;
 	};
 	const SelectorEngine = {
 	  find(selector, element = document.documentElement) {
@@ -7096,7 +7096,7 @@
 	const CLASS_DROPDOWN = 'dropdown';
 	const SELECTOR_DROPDOWN_TOGGLE = '.dropdown-toggle';
 	const SELECTOR_DROPDOWN_MENU = '.dropdown-menu';
-	const NOT_SELECTOR_DROPDOWN_TOGGLE = ':not(.dropdown-toggle)';
+	const NOT_SELECTOR_DROPDOWN_TOGGLE = `:not(${SELECTOR_DROPDOWN_TOGGLE})`;
 	const SELECTOR_TAB_PANEL = '.list-group, .nav, [role="tablist"]';
 	const SELECTOR_OUTER = '.nav-item, .list-group-item';
 	const SELECTOR_INNER = `.nav-link${NOT_SELECTOR_DROPDOWN_TOGGLE}, .list-group-item${NOT_SELECTOR_DROPDOWN_TOGGLE}, [role="tab"]${NOT_SELECTOR_DROPDOWN_TOGGLE}`;
@@ -7513,7 +7513,13 @@
 
 	var toastsTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="toast"]'));
 	toastsTriggerList.map(function (toastTriggerEl) {
-	  return new Toast(toastTriggerEl);
+	  if (!toastTriggerEl.hasAttribute('data-bs-target')) {
+	    return;
+	  }
+	  var toastEl = new Toast(toastTriggerEl.getAttribute('data-bs-target'));
+	  toastTriggerEl.addEventListener('click', function () {
+	    toastEl.show();
+	  });
 	});
 
 	var prefix = 'tblr-';
